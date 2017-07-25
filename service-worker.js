4d48bf7e392c54c5552f7acfaec29f7a2af31adb
Riza Fahmi <rizafahmi@gmail.com>
2017-07-25 14:55:19 +0700

    Cache then network strategy. Cache app shell and data

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
 var cacheName = 'weatherPWA'
+var dataCacheName = 'weatherData-v1'
+
 var filesToCache = [
   '/',
   '/index.html',
@@ -35,7 +37,7 @@ self.addEventListener('activate', function (e) {
     caches.keys().then(function (keyList) {
       return Promise.all(
         keyList.map(function (key) {
-          if (key !== cacheName) {
+          if (key !== cacheName && key !== dataCacheName) {
             console.log('[ServiceWorker] Removing old cache', key)
             return caches.delete(key)
           }
@@ -48,9 +50,28 @@ self.addEventListener('activate', function (e) {
 
 self.addEventListener('fetch', function (e) {
   console.log('[ServiceWorker] Fetch', e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request)
-    })
-  )
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql'
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function (cache) {
+        return fetch(e.request).then(function (response) {
+          cache.put(e.request.url, response.clone())
+          return response
+        })
+      })
+    )
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function (response) {
+        return response || fetch(e.request)
+      })
+    )
+  }
 })
